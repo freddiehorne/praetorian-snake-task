178f5ff391130896e359deaa6d2c3247148faa21
Frederick Horne <frederickhorne@fredericks-macbook-pro.home>
2022-04-03 12:19:16 +0200

    added a sort function to the leaderboard data to make it appear in order of points descending

--- src/components/LeaderboardList.jsx
+++ src/components/LeaderboardList.jsx
@@ -1,9 +1,15 @@
 const LeaderboardList = (props) => {
 	const leaderboardData = props.leaderboardData;
 
+	leaderboardData.sort((item1, item2) => {
+		const first = item1.points;
+		const second = item2.points;
+		return second - first;
+	});
+
 	return leaderboardData.map((person, index) => {
 		return (
-			<div>
+			<div key={index}>
 				<p>{person.name}</p>
 				<p>{person.points}</p>
 			</div>
